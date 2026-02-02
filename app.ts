e07de98f9f335ffef4a25b5998f12c365bc07a13
AdamKoda2306 <adam.koda2306@gmail.com>
2026-02-02 14:28:16 +0530

    initial server setup codes

---
+++ app.ts
@@ -0,0 +1,54 @@
+import express, { Response, Request, NextFunction } from 'express';
+import cors from 'cors';
+import connectDB from './config/db.config';
+
+// ---------------- Routes ---------------------
+// import authRoutes from './routes/auth.routes';
+
+const app = express();
+
+// ---------------- Middleware ------------------
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+
+app.use(express.json());
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log("➡️", req.method, req.url);
+  console.log("IP:", req.ip);
+  console.log("Headers:", req.headers['authorization']);
+  console.log("Params:", req.params);
+  console.log("Query:", req.query);
+  console.log("Body:", req.body);
+  next();
+});
+
+// ---------------- DB Connection -------------------
+connectDB();
+
+// ---------------- Test Routes ---------------------
+app.get('/', async (req: Request, res: Response) => {
+    res.status(200).json({ message: "Server is Working!"});
+});
+
+app.get('/ping', async (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "OK",
+        date: new Date().toString()
+    });
+});
+
+// ---------------- Error Handler --------------------
+app.use((err: Error, req: Request, res: Response) => {
+    console.error(`Error Handler: ${err.message}`);
+    res.status(500).json({ message: "Internal Server Error"});
+});
+
+// ---------------- Actual Routes --------------------
+// app.use("/auth", authRoutes);
+
+export default app;
+
+
